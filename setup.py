95abfbdcc8c233dc8f57a2bad96c934f0bf7cf64
James Hiebert <james@hiebert.name>
2017-12-15 13:58:53 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-__version__ = '3.2.4'
+__version__ = '3.2.5'
 
 install_requires = [
     'Numpy',
